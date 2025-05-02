8114fb9e68fb2a60606e90cb402b54d2f418f52f
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-05-02 14:56:41 -0300

    Add test case to dungeon game

--- dungeon-game/src/main/scala/Main.scala
+++ dungeon-game/src/main/scala/Main.scala
@@ -1,12 +1,19 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    val dungeon = Array(
+    val dungeon1 = Array(
       Array(-2, -3, 3),
       Array(-5, -10, 1),
       Array(10, 30, -5)
     )
 
-    val result = Solution.calculateMinimumHP(dungeon)
-    println(s"Minimum HP needed: $result")
+    val dungeon2 = Array(
+      Array(0)
+    )
+
+    val result1 = Solution.calculateMinimumHP(dungeon1)
+    println(s"Minimum HP needed for dungeon1: $result1")
+
+    val result2 = Solution.calculateMinimumHP(dungeon2)
+    println(s"Minimum HP needed for dungeon2: $result2")
   }
 }
